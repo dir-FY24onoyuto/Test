4835529cb7bbd74993416c79183f65dad63d9829
dir-FY24onoyuto <yuto.ono.t380@dirbato.co.jp>
2024-04-15 16:01:28 +0900

    コミット

--- Test/src/calc/CalculatorTest.java
+++ Test/src/calc/CalculatorTest.java
@@ -3,20 +3,23 @@ package calc;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.ArgumentsSource; 
 
 class CalculatorTest {
-
+    
     @Test
-    void testAdd() {
+    void testadd() {
         Calculator calculator = new Calculator();
-        assertEquals(5, calculator.add(2, 3));
+        assertEquals(5, calculator.add(2,3));
     }
-
+    
     @Test
     void testSubtract() {
-        Calculator calculator = new Calculator();
-        assertEquals(-1, calculator.subtract(2, 3));
-    }
-
+       Calculator calculator = new Calculator();
+       assertEquals(-1, calculator.subtract(2,3));
+       
+      
+   }
 }
+
 
